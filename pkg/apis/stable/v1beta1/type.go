74a30d90b3a56a11474c0e18101ac90f412f79a3
winkyi <winkyi@163.com>
2022-02-08 20:51:45 +0800

    初始化controller

---
+++ pkg/apis/stable/v1beta1/type.go
@@ -0,0 +1,33 @@
+package v1beta1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +genclient:noStatus
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type CronTab struct {
+	metav1.TypeMeta `json:",inline"`
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+	Spec              CronTabSpec `json:"spec"`
+}
+
+// +k8s:deepcopy-gen=false
+
+type CronTabSpec struct {
+	CronSpec string `json:"cronSpec"`
+	Image    string `json:"image"`
+	Replicas int    `json:"replicas"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type CronTabList struct {
+	metav1.TypeMeta `json:",inline"`
+	// +optional
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []CronTab `json:"items"`
+}
